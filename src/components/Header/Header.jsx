228293003ad4c9ef28ce8d651f7e13101b4940bc
WeBstwr <ifedhawebster@gmail.com>
2024-07-14 11:44:33 +0300

    add hamburger menu to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <section className="header">
       <div className="header-container">
         <nav className="header-nav">
-          <ol className="header-nav-list">
+          <ol className={`header-nav-list ${menuOpen ? "open" : ""}`}>
             <li className="header-nav-item">
-              <Link to="/"></Link>
+              <Link to="/">Home</Link>
             </li>
             <li className="header-nav-item">
-              <Link to="/Work">works</Link>
+              <Link to="/Work">Works</Link>
             </li>
             <li className="header-nav-item">
-              <Link to="/Blog">blog</Link>
+              <Link to="/Blog">Blog</Link>
             </li>
             <li className="header-nav-item">
-              <Link to="/Contact">contact</Link>
+              <Link to="/Contact">Contact</Link>
             </li>
           </ol>
+          <button
+            className="header-nav-toggle"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span className="header-nav-toggle-icon"></span>
+          </button>
         </nav>
       </div>
     </section>
