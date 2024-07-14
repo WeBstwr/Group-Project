32e833fa7905b7fac10db7ba2c9fa2a7b25bcce7
Ian Kimani <ian.kiarie2002@gmail.com>
2024-07-14 19:54:58 +0300

    add responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
-import './footer.css'
-
+import "./footer.css";
 
 function Footer() {
   return (
- <section className="footer">
-  <div className="icons">
-    <a className="facebook"><FaFacebook /></a>
-    <a className="instagram"><FaInstagram /></a>
-    <a className="twitter"><FaTwitter /></a>
-    <a className="linkedin"><FaLinkedin /></a>
-  </div>
-  <div className="copyright">
-    <p>Copyright &copy; 2020 all rights reserved</p>
-  </div>
- </section>
-  )
+    <section className="footer">
+      <div className="icons">
+        <a className="facebook">
+          <FaFacebook />
+        </a>
+        <a className="instagram">
+          <FaInstagram />
+        </a>
+        <a className="twitter">
+          <FaTwitter />
+        </a>
+        <a className="linkedin">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div className="copyright">
+        <p>Copyright &copy; 2020 all rights reserved</p>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
